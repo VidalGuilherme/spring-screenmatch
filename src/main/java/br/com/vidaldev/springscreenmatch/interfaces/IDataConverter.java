df8844f07f5c691c4f62a7279b9d90f09845ff3b
Guilherme Fernandes Vidal <guileads@gmail.com>
2024-02-18 14:44:07 -0300

    interface to data converter

--- src/main/java/br/com/vidaldev/springscreenmatch/interfaces/IDataConverter.java
+++ src/main/java/br/com/vidaldev/springscreenmatch/interfaces/IDataConverter.java
@@ -1,6 +1,9 @@
 package br.com.vidaldev.springscreenmatch.interfaces;
 
+import java.util.List;
+
 public interface IDataConverter {
 
     <T> T convertData(String json, Class<T> toClass);
+    <T> List<T> convertList(String json, Class<T> toClass);
 }
--- src/main/java/br/com/vidaldev/springscreenmatch/services/JacksonDataConverter.java
+++ src/main/java/br/com/vidaldev/springscreenmatch/services/JacksonDataConverter.java
@@ -3,6 +3,9 @@ package br.com.vidaldev.springscreenmatch.services;
 import br.com.vidaldev.springscreenmatch.interfaces.IDataConverter;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+
+import java.util.List;
 
 public class JacksonDataConverter implements IDataConverter {
 
@@ -12,6 +15,17 @@ public class JacksonDataConverter implements IDataConverter {
     public <T> T convertData(String json, Class<T> toClass) {
         try {
             return mapper.readValue(json, toClass);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> List<T> convertList(String json, Class<T> toClass){
+        CollectionType list = mapper.getTypeFactory()
+                .constructCollectionType(List.class, toClass);
+
+        try {
+            return mapper.readValue(json, list);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
